6e5f7f99cf175928beb9e24fd1d8dbb66a3940f6
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-11-17 12:00:32 +0330

    Converting icons into separate components

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -1,3 +1,6 @@
+import DeleteIcon from "./icons/DeleteIcon";
+import EditIcon from "./icons/EditIcon";
+
 export default function TodoListItem() {
   return (
     <li class="relative flex items-center justify-between px-2 py-6 border-b">
@@ -11,34 +14,8 @@ export default function TodoListItem() {
         type="button"
         class="absolute right-0 flex items-center space-x-1"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="w-5 h-5 text-blue-600"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="w-5 h-5 text-red-700"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
+        <EditIcon />
+        <DeleteIcon />
       </button>
     </li>
   );
