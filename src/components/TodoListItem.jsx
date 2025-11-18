1b754a71ede57d069874a9382ad0eea513b28739
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-11-18 16:39:05 +0330

    Add line-through class

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -5,8 +5,14 @@ export default function TodoListItem({ todo }) {
   return (
     <li className="relative flex items-center justify-between px-2 py-6 border-b">
       <div>
-        <input type="checkbox" className="" />
-        <p className="inline-block mt-1 ml-2 text-gray-600">{todo?.title}</p>
+        <input type="checkbox" />
+        <p
+          className={`"inline-block mt-1 ml-2 text-gray-600"${
+            todo?.status ? "line-through" : ""
+          }`}
+        >
+          {todo?.title}
+        </p>
       </div>
       <button
         type="button"
